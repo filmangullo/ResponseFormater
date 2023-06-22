3d9ec7cdda714e2e08fe7a598486fcedb54dfe8e
Filimantaptius Gulo <filmantapgullo@gmail.com>
2023-06-22 21:50:24 +0700

    Create ResponseFormatter.go

---
+++ ResponseFormatter.go
@@ -0,0 +1,31 @@
+package ...
+
+type Response struct {
+	Meta Meta        `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+type Meta struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
+}
+
+ **
+ * ResponseFormatter Json API
+ */
+
+func ResponseFormatter(message string, code int, status string, data interface{}) Response {
+	meta := Meta{
+		Message: message,
+		Code:    code,
+		Status:  status,
+	}
+
+	results := Response{
+		Meta: meta,
+		Data: data,
+	}
+
+	return results
+}
